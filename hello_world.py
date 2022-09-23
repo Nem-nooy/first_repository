e76d1e05e181570725244e5ea66a56ee45771b57
Nem-nooy <sgu101310@gmail.com>
2022-09-23 09:31:44 +0900

    Delete hello_world.py

--- hello_world.py
+++
@@ -1,9 +0,0 @@
-def print_hello():
-    print("Hello World!!")
-
-def print_message() : 
-    print("reverse the name for seeing truth.")
-
-if __name__ == '__main__':
-    print_message()
-    print_hello()
