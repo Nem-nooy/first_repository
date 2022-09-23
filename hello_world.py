2206650edbcdd2723df8aadfe177f509c91df2f2
Nem-nooy <sgu101310@gmail.com>
2022-09-23 09:23:42 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,9 @@
 def print_hello():
     print("Hello World!!")
 
-def print_NN() : 
-    print("Nem_nooy")
+def print_message() : 
+    print("reverse the name for seeing truth.")
 
 if __name__ == '__main__':
-    print_NN()
+    print_message()
     print_hello()
